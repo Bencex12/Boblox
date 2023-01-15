07a90c39825930d75e78efdd3a53ce7996da5216
Bencex12 <122749633+Bencex12@users.noreply.github.com>
2023-01-15 20:55:32 +0100

    Update bencex.lua

--- bencex.lua
+++ bencex.lua
@@ -30,7 +30,7 @@ local part = Instance.new("Part",workspace)
     part.BottomSurface = Enum.SurfaceType.Smooth
 
 
-auf:Toggle("Auto-Farm Energy",false, function(state)
+auf:Toggle("Farmolj cigany max kibasznak",false, function(state)
 getgenv().af = state
 
 game:GetService("RunService").RenderStepped:Connect(function()
@@ -47,7 +47,7 @@ game:GetService("RunService").RenderStepped:Connect(function()
 end)
 end)
 
-auf:Toggle("Safe Farm",false, function(state)
+auf:Toggle("Elvileg ezert nem basz ki",false, function(state)
 getgenv().safe = state
 
 player.Character.HumanoidRootPart.CFrame = game:GetService("Workspace"):FindFirstChild("BS").CFrame * CFrame.new(0,10,0)
@@ -90,14 +90,14 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Wo
 end)
 
 
-msc:Textbox("WalkSpeed", "ird ide buzikam!", true, function(v)
+msc:Textbox("Seta Speed Attisnak", "ird ide buzikam!", true, function(v)
      game:GetService("Players").LocalPlayer.chosenSpeed.Value = v
      game:GetService("Players").LocalPlayer.usingChosenSpeed.Value = true
      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
 end)
 
 
-msc:Button("Infinity faszveres", function()
+msc:Button("ez olyan hogy nem guggulsz le", function()
 local InfiniteJumpEnabled = true
 game:GetService("UserInputService").JumpRequest:connect(function()
 	if InfiniteJumpEnabled then
